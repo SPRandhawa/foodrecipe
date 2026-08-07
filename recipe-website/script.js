b3ff32c21474d8a473d59122f2ccc8bae897afd5
SUKHRAJ7483 <sukhrajkaursukhraj281@gmail.com>
2026-08-07 22:56:54 +0530

    Change image URL for Gulab Jamun
    Updated image link for Gulab Jamun recipe.

--- recipe-website/script.js
+++ recipe-website/script.js
@@ -9,7 +9,7 @@ const recipes = [
     {
         name: "Gulab Jamun",
         category: "indiandessert",
-        image: "https://images.unsplash.com/photo-1617093727343-374698b1b08d?w=600",
+        image: "https://www.google.com/search?q=gulab+jamun+image+link&oq=gulab+jamun+image+link&aqs=chrome..69i57j33i160l3.12031j0j7&client=ms-android-xiaomi-rvo2b&sourceid=chrome-mobile&ie=UTF-8",
         steps: "Fry dough balls and soak in sugar syrup.",
         youtube: "https://youtu.be/eQPZv_bwKGA?si=Sm4zMDWbbjE1i97J",
     },
